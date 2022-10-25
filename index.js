ad436f9f69e2d26cc4a9a9acb4ff6e7282ddd5f2
unknown <textile.zero@gmail.com>
2022-10-26 05:36:46 +0600

    course details based on course id api configured

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 // initialization
 const express = require("express");
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
+// cors
+const cors = require("cors");
+app.use(cors());
+
 // getting data
 const categories = require("./data/category.json");
 const courses = require("./data/courses.json");
@@ -10,10 +14,15 @@ const courses = require("./data/courses.json");
 app.get("/categories", (req, res) => {
     res.send(categories);
 });
-
 // courses api
 app.get("/courses", (req, res) => {
     res.send(courses)
+});
+// course details api
+app.get("/course/:id", (req, res) => {
+    const courseId = req.params.id;
+    const selectedCourse = courses.find(course => course.id === parseInt(courseId));
+    res.send(selectedCourse);
 })
 
 app.listen(port, () => {
