f4313b93f29e3ee7cdd1e63dbaa2b9a3b7fbd6b3
unknown <textile.zero@gmail.com>
2022-10-25 23:38:09 +0600

    courses and category api configured

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
+// initialization
 const express = require("express");
 const app = express();
 const port = 5000;
+// getting data
+const categories = require("./data/category.json");
+const courses = require("./data/courses.json");
 
-app.get("/", (req, res) => {
-    res.send('Hello World');
+// course categories api
+app.get("/categories", (req, res) => {
+    res.send(categories);
+});
+
+// courses api
+app.get("/courses", (req, res) => {
+    res.send(courses)
 })
 
 app.listen(port, () => {
