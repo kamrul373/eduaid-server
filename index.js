d5e6ac35c047874ef1ddc4d31b1a6c4913d8579a
unknown <textile.zero@gmail.com>
2022-10-27 20:23:41 +0600

    server issue fiexd

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ app.use(cors());
 // getting data
 const categories = require("./data/category.json");
 const courses = require("./data/courses.json");
+const course_type = require("./data/courseType.json");
 
 // course categories api
 app.get("/categories", (req, res) => {
@@ -19,20 +20,34 @@ app.get("/courses", (req, res) => {
     res.send(courses)
 });
 // course details api
-app.get("/courses/:id", (req, res) => {
-    const courseId = req.params.id;
+app.get("/courses-details/:id", (req, res) => {
+    const courseId = req.params.id
     const selectedCourse = courses.find(course => course.id === parseInt(courseId));
-    res.send(selectedCourse);
+    res.send(selectedCourse)
 });
+// courses based on course type
+app.get("/course-type/:type", (req, res) => {
+    const type = req.params.type;
+    const selectedCourses = courses.filter(course => course.info.course_type.toLowerCase() === type.toLowerCase());
+    res.send(selectedCourses);
+});
+// course type
+app.get("/course-type", (req, res) => {
+    res.send(course_type);
+})
 
 // courses on a category
-
 app.get("/categories/:id", (req, res) => {
-    const categoryId = req.params.id;
-    const categoryCourses = courses.filter(course => course.category_id === parseInt(categoryId));
+    const categoryId = parseInt(req.params.id)
+    if (categoryId === 10) {
+        return res.send(courses);
+    } else {
+        const categoryCourses = courses.filter(course => course.category_id === categoryId);
+        res.send(categoryCourses);
+    }
 
-    res.send(categoryCourses);
-})
+});
+
 
 app.listen(port, () => {
     console.log("EDU AID server is running at port : ", port);
